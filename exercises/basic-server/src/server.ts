0e776224cecc9dbe318ed595b183099d48a0b547
Srivathsan Srinivasagopalan <srivathsan.srinivas@gmail.com>
2020-11-09 20:22:21 -0600

    adding server stuff

--- exercises/basic-server/src/server.ts
+++ exercises/basic-server/src/server.ts
@@ -1,31 +1,31 @@
-import bodyParser from "body-parser";
-import express, { Request, Response, Router } from "express";
+import express, { Router, Request, Response } from 'express';
+import bodyParser from 'body-parser';
 
-import { Car, cars as cars_list } from "./cars";
+import { Car, cars as cars_list } from './cars';
 
 (async () => {
-  const cars: Car[]  = cars_list;
+  let cars:Car[]  = cars_list;
 
-  // Create an express applicaiton
-  const app = express();
-  // default port to listen
-  const port = 8082;
-
-  // use middleware so post bodies
-  // are accessable as req.body.{{variable}}
-  app.use(bodyParser.json());
+  //Create an express applicaiton
+  const app = express(); 
+  //default port to listen
+  const port = 8082; 
+  
+  //use middleware so post bodies 
+  //are accessable as req.body.{{variable}}
+  app.use(bodyParser.json()); 
 
   // Root URI call
   app.get( "/", ( req: Request, res: Response ) => {
     res.status(200).send("Welcome to the Cloud!");
   } );
 
-  // Get a greeting to a specific person
+  // Get a greeting to a specific person 
   // to demonstrate routing parameters
   // > try it {{host}}/persons/:the_name
-  app.get( "/persons/:name",
+  app.get( "/persons/:name", 
     ( req: Request, res: Response ) => {
-      const { name } = req.params;
+      let { name } = req.params;
 
       if ( !name ) {
         return res.status(400)
@@ -39,7 +39,7 @@ import { Car, cars as cars_list } from "./cars";
   // Get a greeting to a specific person to demonstrate req.query
   // > try it {{host}}/persons?name=the_name
   app.get( "/persons/", ( req: Request, res: Response ) => {
-    const { name } = req.query;
+    let { name } = req.query;
 
     if ( !name ) {
       return res.status(400)
@@ -52,9 +52,9 @@ import { Car, cars as cars_list } from "./cars";
 
   // Post a greeting to a specific person
   // to demonstrate req.body
-  // > try it by posting {"name": "the_name" } as
+  // > try it by posting {"name": "the_name" } as 
   // an application/json body to {{host}}/persons
-  app.post( "/persons",
+  app.post( "/persons", 
     async ( req: Request, res: Response ) => {
 
       const { name } = req.body;
@@ -68,81 +68,83 @@ import { Car, cars as cars_list } from "./cars";
                 .send(`Welcome to the Cloud, ${name}!`);
   } );
 
-  // Add an endpoint to GET a list of cars. it should be filterable by make with a query paramater
-    // > try it {{host}}/cars?make=<make of the car>
-  app.get( "/cars/", ( req: Request, res: Response) => {
-      const { make } = req.query;
+  // @TODO Add an endpoint to GET a list of cars
+  // it should be filterable by make with a query paramater
+  app.get("/cars/", ( req: Request, res: Response ) => {
+      // destruct our query parameters
+      let { make } = req.query;
+      let cars_list = cars;
 
-      // tslint:disable-next-line:no-shadowed-variable
-      let carsList = cars;
-
-      if (! make ) {
-            return res.status(400).send(`Make of the car is required.`);
-        }
-
+      // if we have an optional query parameter, filter by it
       if (make) {
-          carsList = cars.filter((car) => car.make === make);
+          cars_list = cars.filter((car) => car.make === make);
       }
 
-      return res.status(200).send(carsList);
-  });
+      //return the resulting list along with 200 success
+      res.status(200).send(cars_list);
+  }
+  );
+
+
 
   // @TODO Add an endpoint to get a specific car
   // it should require id
   // it should fail gracefully if no matching car is found
-  app.get( "/cars/:id", ( req: Request, res: Response) => {
-      const { id } = req.params;
 
-      if (!id ) {
-          return res.status(400).send(`ID of the car is required.`);
+  app.get( "/cars/:id", (req: Request, res) => {
+      // destruct our path params
+      let { id } = req.params;
+
+      //check to make sure the id is set
+      if (!id) {
+          //respond with an error if not
+          return res.status(400).send(`id is required`);
       }
 
       // try to find the car by id
-      // tslint:disable-next-line:no-shadowed-variable
-      // @ts-ignore
-      // tslint:disable-next-line:no-shadowed-variable
-      const car = car.filter((car) => car.id === id);
+      const car = cars.filter((car) => car.id == id);
 
       // response not found, if we do not have this id
-      if (car && car.length === 0) {
-          return res.status(404).send(`Car is not found`);
+      if(car && car.length === 0) {
+          return res.status(404).send(`car is not found`);
       }
 
-      return res.status(200).send(car);
-  });
+      //return the car with a success status code
+      res.status(200).send(car)
+  } );
 
   /// @TODO Add an endpoint to post a new car to our list
   // it should require id, type, model, and cost
-  // @ts-ignore
+  app.post( "/cars/", (req: Request, res) => {
+      //destruct our body payload for our variables
+      let { make, type, model, cost, id } = req.body;
 
-  app.post( "/cars/", ( req: Request, res: Response ) => {
-      // destruct our body payload for our variables
-      const {make, type, model, cost, id} = req.body;
-
-      // check to make sure all required variables are set
+      //check to make sure all required variables are set
       if (!id || !type || !model || !cost) {
-          return res.status(400).send(`make, type, model, cost, id are required`);
+          //respond with an error if not
+          return res.status(400).send(`make, type, model, cost, id are required.`);
       }
 
-      // create a new car instance
-      // tslint:disable-next-line:variable-name
+      // Create a new car instance
       const new_car: Car = {
-          // tslint:disable-next-line:object-literal-sort-keys
-          make, type, model, cost, id,
+          make: make, type: type, model:model, cost: cost, id:id
       };
+
+      // add thi scar to our local variable
+      cars.push(new_car);
 
       // add this car to our local variable
       cars.push(new_car);
 
-      // send the complete car object as a response along with 201 - creation process
+      // send the complete car object as a response
+      // along with 201 - creation success
       res.status(201).send(new_car);
-  };
+
+  } );
 
   // Start the Server
   app.listen( port, () => {
-      // tslint:disable-next-line:no-console
       console.log( `server running http://localhost:${ port }` );
-      // tslint:disable-next-line:no-console
       console.log( `press CTRL+C to stop server` );
-    } );
+  } );
 })();
